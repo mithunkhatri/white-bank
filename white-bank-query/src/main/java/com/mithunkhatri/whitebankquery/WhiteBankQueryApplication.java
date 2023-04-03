ddabaec3b22cf6bb8f916907e9b8ef260c8a6d06
Mithun Khatri <mk@Mithuns-MacBook-Air.local>
2023-04-03 08:49:08 +0530

    Initial commit for White Bank Query Service

---
+++ white-bank-query/src/main/java/com/mithunkhatri/whitebankquery/WhiteBankQueryApplication.java
@@ -0,0 +1,13 @@
+package com.mithunkhatri.whitebankquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteBankQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteBankQueryApplication.class, args);
+	}
+
+}
---
+++ white-bank-query/src/test/java/com/mithunkhatri/whitebankquery/WhiteBankQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithunkhatri.whitebankquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteBankQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
