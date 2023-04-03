46047c8c7913ef05347474be1e7ecec150a4a8ef
Mithun Khatri <mk@Mithuns-MacBook-Air.local>
2023-04-03 08:27:50 +0530

    Initial commit for White Bank Command Service

---
+++ white-bank-cmd/src/main/java/com/mithunkhatri/whitebankcmd/WhiteBankCmdApplication.java
@@ -0,0 +1,13 @@
+package com.mithunkhatri.whitebankcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteBankCmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteBankCmdApplication.class, args);
+	}
+
+}
---
+++ white-bank-cmd/src/test/java/com/mithunkhatri/whitebankcmd/WhiteBankCmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithunkhatri.whitebankcmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteBankCmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
